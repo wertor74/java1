00d9d6ea5a8232513303692825562c2305c2453b
wertor <wertor74@gmail.com>
2020-11-11 20:32:31 +0300

    Test1

--- src/ru/progwards/java1/SeaBattle/wertor74/SeaBattleAlg.java
+++ src/ru/progwards/java1/SeaBattle/wertor74/SeaBattleAlg.java
@@ -30,19 +30,6 @@ public class SeaBattleAlg {
     //         7|X|.|X|.|.|.|.|Х|.|X|
     //         8|X|.|.|.|.|.|.|X|.|.|
     //         9|X|.|.|.|X|.|.|.|.|.|
-//    static char [][] field; // своё поле для отмечания выстрелов
-
-//    public void init (SeaBattle seaBattle) {
-//        field = new char[seaBattle.getSizeY()][seaBattle.getSizeX()];
-//        for (int i = 0; i < seaBattle.getSizeY(); i++) {
-//            Arrays.fill(field[i], '0');
-//        }
-//    }
-//    public void encircleDestroyed (int x, int y) { //обводим убитые корабли
-//        //System.out.println("x = " + x + ", y = " + y);
-//        if (y < 9) field[x][y + 1] = '.';
-//        if (x < 9) field[x + 1][y] = '.';
-//    }
 
     public void battleAlgorithm(SeaBattle seaBattle) {
         // своё поле для отмечания выстрелов
@@ -50,57 +37,162 @@ public class SeaBattleAlg {
         for (int i = 0; i < seaBattle.getSizeY(); i++) {
             Arrays.fill(field[i], '0');
         }
+        class Miss { // Локальный класс для обработки "мимо"
+            Miss(int x, int y) {
+                field[x][y] = '*';
+            }
+        }
+        class Destroyed { // Локальный класс для обработки "убит"
+            Destroyed(int x, int y, int len, boolean isVertical, boolean isHorizontal) {
+                field[x][y] = 'X';
+                len++;
+                if (len == 0) { // обводим однопалубный
+                    if (y < 9) {
+                        switch (x) {
+                            case 0:
+                                field[x + 1][y] = '.';
+                                field[x + 1][y +1] = '.';
+                                field[x][y + 1] = '.';
+                                break;
+                            case 9:
+                                field[x][y + 1] = '.';
+                                field[x - 1][y + 1] = '.';
+                                break;
+                            default:
+                                field[x + 1][y] = '.';
+                                field[x + 1][y + 1] = '.';
+                                field[x][y + 1] = '.';
+                                field[x - 1][y + 1] = '.';
+                        }
+                    } else {
+                        if (x < 9) field[x + 1][y] = '.';
+                    }
+                }
+                if (isHorizontal == true) { // многопалубный, если горизонтальный
+                    switch (y) {
+                        case 9:
+                            if (x < 9) field[x + 1][y] = '.';
+                            break;
+                        default:
+                            if (x == 9) {
+                                for (int i = 0; i <= len; i++) {
+                                    if (field[x - i][y + 1] == '0')field[x - i][y + 1] = '.';
+                                }
+                            } else if (x - (len - 1) == 0) {
+                                field[x + 1][y] = '.';
+                                for (int i = 0; i <= len; i++) {
+                                    if (field[i][y + 1] == '0') field[i][y + 1] = '.';
+                                }
+                            } else {
+                                field[x + 1][y] = '.';
+                                field[x + 1][y + 1] = '.';
+                                for (int i = 0; i <= len; i++) {
+                                    if (field[x - i][y + 1] == '0') field[x - i][y + 1] = '.';
+                                }
+                            }
+                    }
+                } else { // многопалубный, если вертикальный
+                    switch (y) {
+                        case 9:
+                            if (x == 0) {
+                                for (int i = 0; i <= (len - 1); i++) {
+                                    field[x + 1][y - i] = '.';
+                                }
+                            } else if (x == 9) {
+                                for (int i = 0; i <= (len - 1); i++) {
+                                    if (field[x - 1][y - i] == '0') field[x - 1][y - i] = '.';
+                                }
+                            } else {
+                                for (int i = 0; i <= (len - 1); i++) {
+                                    if (field[x - 1][y - i] == '0') field[x - 1][y - i] = '.';
+                                    if (field[x + 1][y - i] == '0') field[x + 1][y - i] = '.';
+                                }
+                            }
+                            break;
+                        default:
+                            if (x == 0) {
+                                if (field[x][y + 1] == '0') field[x][y + 1] = '.';
+                                if (field[x + 1][y + 1] == '0') field[x + 1][y + 1] = '.';
+                                for (int i = 0; i <= (len - 1); i++) {
+                                    if (field[x + 1][y - i] == '0') field[x + 1][y - i] = '.';
+                                }
+                            } else if (x == 9) {
+                                if (field[x][y + 1] == '0') field[x][y + 1] = '.';
+                                if (field[x - 1][y + 1] == '0') field[x - 1][y + 1] = '.';
+                                for (int i = 0; i <= (len - 1); i++) {
+                                    if (field[x - 1][y - i] == '0') field[x - 1][y - i] = '.';
+                                }
+                            } else {
+                                if (field[x - 1][y + 1] == '0') field[x - 1][y + 1] = '.';
+                                if (field[x][y + 1] == '0') field[x][y + 1] = '.';
+                                if (field[x + 1][y +1] == '0') field[x + 1][y +1] = '.';
+                                for (int i = 0; i <= (len - 1); i++) {
+                                    if (field[x - 1][y - i] == '0') field[x - 1][y - i] = '.';
+                                    if (field[x + 1][y - i] == '0') field[x + 1][y - i] = '.';
+                                }
+                            }
+                    }
+                }
+            }
+        }
+        class Hit { // Локальный класс для обработки "ранил"
+            Hit(int x, int y, int len, boolean isVertical, boolean isHorizontal) {
+                field[x][y] = 'X';
+                len ++;
+                if (y < 9) {
+                    switch (seaBattle.fire(x, y + 1)) {
+                        case MISS:
+                            new Miss(x, y + 1);
+                            isHorizontal = true;
+                            int i = 1;
+                            while (seaBattle.fire(x + i, y) == SeaBattle.FireResult.HIT) {
+                                field[x + i][y] = 'X';
+                                len++;
+                                i++;
+                            }
+                            new Destroyed(x + i, y, len, isVertical, isHorizontal);
+                            break;
+                        case HIT:
+                            new Hit(x, y + 1, len, isVertical, isHorizontal);
+                            isVertical = true;
+                            int j = 2;
+                            while (seaBattle.fire(x, y + j) == SeaBattle.FireResult.HIT) {
+                                field[x][y + j] = 'X';
+                                len++;
+                                j++;
+                            }
+                                new Destroyed(x, y + j, len, isVertical, isHorizontal);
+                            break;
+                        case DESTROYED:
+                            new Destroyed(x, y + 1, len, isVertical, isHorizontal);
+                    }
+                }
+            }
+        }
+        int hits = 0; // считаем количество подбитых кораблей
+        int len = 0; // длинна корабля
+        boolean isVertical = false; // положение раненного корабля (вертикальный)
+        boolean isHorizontal = false; // положение корабля (горизонтальный)
         // пример алгоритма:
         // стрельба по всем квадратам поля полным перебором
-        int hits = 0; // считаем количество подбитых клеток
         for (int y = 0; y < seaBattle.getSizeX(); y++) {
             for (int x = 0; x < seaBattle.getSizeY(); x++) {
+                // проверяем количество подбитых кораблей
+                if (hits >= 10) return;
                 // проверяем пустая ячейка, или нет. Если не пустая стреляем в следующую
                 if (field[x][y] != '0') continue;
                 SeaBattle.FireResult fireResult = seaBattle.fire(x, y);
                 switch (fireResult) {
                     case MISS:
-                        field[x][y] = '*';
+                        new Miss(x, y);
                         break;
                     case HIT:
-                        field[x][y] = 'X';
                         hits++;
+                        new Hit(x, y, len, isVertical, isHorizontal);
                         break;
                     case DESTROYED:
-                        field[x][y] = 'X';
-                        if (y < 9) {
-                            switch (x) {
-                                case 0:
-                                    field[x + 1][y] = '0';
-                                    field[x + 1][y +1] = '0';
-                                    field[x][y + 1] = '0';
-                                    break;
-                                case 9:
-                                    field[x][y + 1] = '.';
-                                    field[x - 1][y + 1] = '.';
-                                    break;
-                                default:
-                                    field[x + 1][y] = '.';
-                                    field[x + 1][y + 1] = '.';
-                                    field[x][y + 1] = '.';
-                                    field[x - 1][y + 1] = '.';
-                            }
-                        } else {
-                            if (x < 9) field[x + 1][y] = '.';
-                        }
-
-                        System.out.println(seaBattle);
-                        for (int k = 0; k <= 9; k++) {
-                            for (int l = 0; l <= 9; l++) {
-                                if (l < 9) {
-                                    System.out.print(field[l][k]);
-                                } else {
-                                    System.out.println(field[l][k]);
-                                }
-                            }
-                        }
                         hits++;
-                        if (hits >= 20) return;
+                        new Destroyed(x, y, len, isVertical, isHorizontal);
                 }
             }
         }
