344bcc625168dc6adfa3db5cf64b1e4fe80c1c61
wertor <wertor74@gmail.com>
2020-11-23 23:55:53 +0300

    Test1

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -29,6 +29,18 @@ public class ArrayInteger {
         return new BigInteger(strToInt);
     }
     boolean add(ArrayInteger num) {
+        // сравниваем длинну массивов
+        if (digits.length > num.digits.length) {
+            BigInteger valueAdd = this.toInt();
+            this.digits = Arrays.copyOf(digits, num.digits.length);
+            this.fromInt(valueAdd);
+            System.out.println(Arrays.toString(digits));
+        } else if (num.digits.length > digits.length) {
+            BigInteger valueAdd = num.toInt();
+            num.digits = Arrays.copyOf(num.digits, digits.length);
+            num.fromInt(valueAdd);
+        }
+        // складываем числа
         for (int i = digits.length; i > 0 ; i--) {
             switch (i) {
                 case 1:
@@ -50,12 +62,12 @@ public class ArrayInteger {
     }
 
     public static void main(String[] args) {
-        ArrayInteger arrayInteger = new ArrayInteger(6);
-        arrayInteger.fromInt(new BigInteger("888888"));
+        ArrayInteger arrayInteger = new ArrayInteger(5);
+        arrayInteger.fromInt(new BigInteger("88888"));
         System.out.println(Arrays.toString(arrayInteger.digits));
         System.out.println(arrayInteger.toInt() + ", " + arrayInteger.toInt().getClass());
-        ArrayInteger num = new ArrayInteger(6);
-        num.fromInt(new BigInteger("111112"));
+        ArrayInteger num = new ArrayInteger(4);
+        num.fromInt(new BigInteger("1111"));
         System.out.println(Arrays.toString(num.digits));
         System.out.println(num.toInt() + ", " + num.toInt().getClass());
         System.out.println(arrayInteger.add(num) + ", " + Arrays.toString(arrayInteger.digits));
