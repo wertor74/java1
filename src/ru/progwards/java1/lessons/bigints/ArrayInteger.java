288fbb17934e046424aca154b7970db365f70be8
wertor <wertor74@gmail.com>
2020-11-24 21:19:20 +0300

    Test1

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -29,16 +29,17 @@ public class ArrayInteger {
         return new BigInteger(strToInt);
     }
     boolean add(ArrayInteger num) {
-        // сравниваем длинну массивов
+        // сравниваем длинну массивов и приводим к большей длинне
         if (digits.length > num.digits.length) {
-            BigInteger valueAdd = this.toInt();
-            this.digits = Arrays.copyOf(digits, num.digits.length);
-            this.fromInt(valueAdd);
-            System.out.println(Arrays.toString(digits));
-        } else if (num.digits.length > digits.length) {
             BigInteger valueAdd = num.toInt();
             num.digits = Arrays.copyOf(num.digits, digits.length);
+            Arrays.fill(num.digits, (byte) 0);
             num.fromInt(valueAdd);
+        } else if (num.digits.length > digits.length) {
+            BigInteger valueAdd = num.toInt();
+            digits = Arrays.copyOf(digits, num.digits.length);
+            Arrays.fill(digits, (byte) 0);
+            this.fromInt(valueAdd);
         }
         // складываем числа
         for (int i = digits.length; i > 0 ; i--) {
@@ -62,12 +63,12 @@ public class ArrayInteger {
     }
 
     public static void main(String[] args) {
-        ArrayInteger arrayInteger = new ArrayInteger(5);
-        arrayInteger.fromInt(new BigInteger("88888"));
+        ArrayInteger arrayInteger = new ArrayInteger(8);
+        arrayInteger.fromInt(new BigInteger("18096253"));
         System.out.println(Arrays.toString(arrayInteger.digits));
         System.out.println(arrayInteger.toInt() + ", " + arrayInteger.toInt().getClass());
-        ArrayInteger num = new ArrayInteger(4);
-        num.fromInt(new BigInteger("1111"));
+        ArrayInteger num = new ArrayInteger(6);
+        num.fromInt(new BigInteger("170851"));
         System.out.println(Arrays.toString(num.digits));
         System.out.println(num.toInt() + ", " + num.toInt().getClass());
         System.out.println(arrayInteger.add(num) + ", " + Arrays.toString(arrayInteger.digits));
