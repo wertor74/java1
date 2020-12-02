c42561f185bd33e645af4abb1426353d8fbe5f1f
wertor <wertor74@gmail.com>
2020-12-03 00:34:46 +0300

    Test1

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -11,32 +11,18 @@ public class CharFilter {
     public static void filterFile(String inFileName, String outFileName, String filter) throws Exception {
         byte[] byteFilter = {};
         byte[] byteText = {};
-        try {
             // читаем данные из входного файла в байтовый массив
             FileInputStream byteFile = new FileInputStream(inFileName);
             // записываем строку filter в файл, чтобы преобразовать её к байтам
             FileWriter fileWriter = new FileWriter("filefilter.txt");
-            try {
                 byteText = byteFile.readAllBytes();
                 fileWriter.write(filter);
-            } finally {
                 byteFile.close();
                 fileWriter.close();
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        try {
             // читаем filter из файла в байтовый массив
             FileInputStream byteFilterFile = new FileInputStream("filefilter.txt");
-            try {
                 byteFilter = byteFilterFile.readAllBytes();
-            } finally {
                 byteFilterFile.close();
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
         // собственно фильтруем
         for (int i = 0; i < byteText.length; i++) {
             for (int j = 0; j < byteFilter.length; j++) {
@@ -46,20 +32,13 @@ public class CharFilter {
             }
         }
         // записываем в файл
-        try {
-            FileOutputStream byteFile = new FileOutputStream(outFileName);
-            try {
-                for (int i = 0; i < byteText.length; i++) {
-                    if (byteText[i] != 0) {
-                        byteFile.write(byteText[i]);
-                    }
-                }
-            } finally {
-                byteFile.close();
+        FileOutputStream byteFile1 = new FileOutputStream(outFileName);
+        for (int i = 0; i < byteText.length; i++) {
+            if (byteText[i] != 0) {
+                byteFile1.write(byteText[i]);
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
         }
+        byteFile1.close();
     }
 
     public static void main(String[] args) throws Exception{
