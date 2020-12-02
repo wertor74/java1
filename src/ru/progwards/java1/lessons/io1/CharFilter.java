0963e391269616b79f2b836359306203ed5b8d22
wertor <wertor74@gmail.com>
2020-12-03 00:44:06 +0300

    Test1

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -1,14 +1,11 @@
 package ru.progwards.java1.lessons.io1;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class CharFilter {
-    public static void filterFile(String inFileName, String outFileName, String filter) throws Exception {
+    public static void filterFile(String inFileName, String outFileName, String filter) throws IOException {
         byte[] byteFilter = {};
         byte[] byteText = {};
             // читаем данные из входного файла в байтовый массив
