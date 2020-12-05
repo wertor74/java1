e1f6dd588545c27245547fbc07235e5a961ca34d
wertor <wertor74@gmail.com>
2020-12-05 22:23:58 +0300

    Test1

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -5,43 +5,41 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class CharFilter {
-    public static void filterFile(String inFileName, String outFileName, String filter) throws IOException {
-        byte[] byteFilter = {};
-        byte[] byteText = {};
-            // читаем данные из входного файла в байтовый массив
-            FileInputStream byteFile = new FileInputStream(inFileName);
-            // записываем строку filter в файл, чтобы преобразовать её к байтам
-            FileWriter fileWriter = new FileWriter("filefilter.txt");
-                byteText = byteFile.readAllBytes();
-                fileWriter.write(filter);
-                byteFile.close();
-                fileWriter.close();
-            // читаем filter из файла в байтовый массив
-            FileInputStream byteFilterFile = new FileInputStream("filefilter.txt");
-                byteFilter = byteFilterFile.readAllBytes();
-                byteFilterFile.close();
+    public static void filterFile(String inFileName, String outFileName, String filter) throws Exception {
+        String text = "";
+        // читаем данные из входного файла в байтовый массив
+        FileReader reader = new FileReader(inFileName);
+        Scanner scanner = new Scanner(reader);
+        while (scanner.hasNextLine()) {
+            text = scanner.nextLine();
+        }
+        reader.close();
+        // представляем текст из входного файла в виде массива символов
+        char[] chrText = text.toCharArray();
+        // представляем filter в виде массива символов
+        char[] chrFilter = filter.toCharArray();
         // собственно фильтруем
-        for (int i = 0; i < byteText.length; i++) {
-            for (int j = 0; j < byteFilter.length; j++) {
-                if (byteText[i] == byteFilter[j]) {
-                    byteText[i] = 0; // обнуляем символ, равный символу из filter
+        for (int i = 0; i < chrText.length; i++) {
+            for (int j = 0; j < chrFilter.length; j++) {
+                if (chrText[i] == chrFilter[j]) {
+                    chrText[i] = '\u0000'; // обнуляем символ, равный символу из filter
                 }
             }
         }
         // записываем в файл
-        FileOutputStream byteFile1 = new FileOutputStream(outFileName);
-        for (int i = 0; i < byteText.length; i++) {
-            if (byteText[i] != 0) {
-                byteFile1.write(byteText[i]);
+        FileWriter writer = new FileWriter(outFileName);
+        for (int i = 0; i < chrText.length; i++) {
+            if (chrText[i] != '\u0000') {
+                writer.write(chrText[i]);
             }
         }
-        byteFile1.close();
+        writer.close();
     }
 
     public static void main(String[] args) throws Exception{
         String inFileName = "filein.txt";
         String outFileName = "fileout.txt";
-        String filter = " -,.()";
+        String filter = " -,.()р";
         filterFile(inFileName, outFileName, filter);
     }
 }
