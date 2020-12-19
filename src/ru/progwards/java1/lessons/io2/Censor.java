5398c1dfc315fce97d597487ca330b827d00c7d4
wertor <wertor74@gmail.com>
2020-12-19 23:50:47 +0300

    Test1

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -1,11 +1,56 @@
 package ru.progwards.java1.lessons.io2;
 
-public class Censor {
-    public static void censorFile(String inoutFileName, String[] obscene) {
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Censor {
+    public static class CensorException extends RuntimeException {
+        public String inoutFileName = "";
+        public CensorException(String inoutFileName) {
+            this.inoutFileName = inoutFileName;
+        }
+        @Override
+        public String getMessage() {
+            return inoutFileName;
+        }
+    }
+    public static void censorFile(String inoutFileName, String[] obscene) {
+        StringBuilder text = new StringBuilder();
+        // читаем файл в строку
+        try (FileReader fr = new FileReader(inoutFileName)) {
+            Scanner sc = new Scanner(fr);
+            while (sc.hasNextLine()) {
+                text.append(sc.nextLine());
+            }
+            // ищем совпадения и меняем на *
+            for (int i = 0; i < obscene.length; i++) {
+                int iO = text.indexOf(obscene[i]);
+                if (iO != -1) {
+                    StringBuilder obs = new StringBuilder("");
+                    for (char c : obscene[i].toCharArray()) {
+                        c = '*';
+                        obs.append(c);
+                    }
+                    text.replace(iO, iO + obscene[i].length(), obs.toString());
+                }
+            }
+            // записываем в файл
+            FileWriter fw = new FileWriter(inoutFileName);
+                fw.write(text.toString());
+        } catch (Exception e) {
+            throw new CensorException(e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
-
+        String inoutFileName = "aaaaaa.txt";
+        String[] obscene = {"Java", "Oracle", "Sun", "Microsystems"};
+        try {
+            censorFile(inoutFileName, obscene);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
