486ab51c6e3ac1e6e1543b0f22ff839992b2a8eb
wertor <wertor74@gmail.com>
2021-01-08 23:28:32 +0300

    Test1

--- src/ru/progwards/java1/lessons/collections/ArrayIterator.java
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -3,22 +3,37 @@ package ru.progwards.java1.lessons.collections;
 import java.util.Iterator;
 
 public class ArrayIterator<T> implements Iterator<T> {
-
     private T[] array;
+    int count;
 
     ArrayIterator(T[] array) {
         this.array = array;
+        count = -1;
     }
 
     @Override
     public boolean hasNext() {
         // TODO Auto-generated method stub
+        count++;
+        if (count < array.length) return true;
         return false;
     }
 
     @Override
     public T next() {
         // TODO Auto-generated method stub
-        return null;
+        return array[count];
+    }
+
+    public static void main(String[] args) {
+        String[] array = {"один", "два", "три", "четыре", "пять"};
+        ArrayIterator arrayIterator = new ArrayIterator(array);
+        for (int i = 0; i < arrayIterator.array.length; i++) {
+            System.out.println("array[" + i + "] = " + arrayIterator.hasNext());
+            System.out.println("array[" + i + "] = " + arrayIterator.next());
+        }
+        while (arrayIterator.hasNext()) {
+            System.out.println(arrayIterator.next());
+        }
     }
 }
