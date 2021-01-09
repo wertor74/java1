4ee4e836c65f5b34d63c03a4c12d2186317034f0
wertor <wertor74@gmail.com>
2021-01-09 23:50:08 +0300

    Test1

--- src/ru/progwards/java1/lessons/collections/MatrixIterator.java
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -1,6 +1,5 @@
 package ru.progwards.java1.lessons.collections;
 
-import java.util.Arrays;
 import java.util.Iterator;
 
 public class MatrixIterator<T> implements Iterator {
@@ -14,7 +13,6 @@ public class MatrixIterator<T> implements Iterator {
     @Override
     public boolean hasNext() {
         if (count2 + 1 < array[count1].length) {
-            count2++;
             return true;
         } else if (count1 < array.length - 1) {
             count1++;
@@ -25,16 +23,23 @@ public class MatrixIterator<T> implements Iterator {
     }
     @Override
     public T next() {
-        return array[count1][count2];
+        if (count2 + 1 < array[count1].length) {
+            count2++;
+            return array[count1][count2];
+        } else if (count1 < array.length - 1) {
+            count1++;
+            count2 = 0;
+            return array[count1][count2];
+        }
+        return null;
     }
-
     public static void main(String[] args) {
         String[] a = {"00", "01", "02", "03", "04"};
         String[] a1 = {"10", "11", "12", "13", "14"};
         String[] a2 = {"20", "21", "22", "23", "24"};
         String[] a3 = {"30", "31", "32", "33", "34"};
         String[] a4 = {"40", "41", "42", "43", "44"};
-        String[][] array = {a, a1, a2};
+        String[][] array = {a, a1, a2, a3, a4};
         MatrixIterator matrixIterator = new MatrixIterator(array);
         while (matrixIterator.hasNext()) {
             System.out.print(matrixIterator.next() + ", ");
