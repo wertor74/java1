5dbecbab1ae1f9d723c3f877b231e1fa5ef1a0ec
wertor <wertor74@gmail.com>
2021-05-25 23:01:00 +0300

    Test1

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -3,23 +3,25 @@ package ru.progwards.java1.lessons.datetime;
 import java.util.*;
 
 public class Profiler {
-    public static HashMap <String, Integer> sectionCount = new HashMap<>(); // словарь для количества вызовов
-    public static HashMap <String, Long> sectionStart = new HashMap<>(); // словарь для времени старта
-    public static HashMap <String, Long> sectionFinish = new HashMap<>(); // словарь для времени финиша
-    public static HashMap <String, Integer> sectionFullTime = new HashMap<>(); // словарь для фултайм
-    public static HashMap <String, Integer> sectionSelfTime = new HashMap<>(); // словарь для селфтайм
+    public static Map <String, Integer> sectionCount = new TreeMap<>(); // словарь для количества вызовов
+    public static Map <String, Long> sectionStartFull = new TreeMap<>(); // словарь для времени старта фулл
+    public static Map <String, Long> sectionStartSelf = new TreeMap<>(); // словарь для времени старта селф
+    public static Map <String, Long> sectionFinish = new TreeMap<>(); // словарь для времени финиша
+    public static Map <String, Integer> sectionFullTime = new TreeMap<>(); // словарь для фуллтайм
+    public static Map <String, Integer> sectionSelfTime = new TreeMap<>(); // словарь для селфтайм
     public static void enterSection(String name) {
         long start = System.currentTimeMillis();
         if (!Profiler.sectionCount.containsKey(name)) {
             Profiler.sectionCount.put(name, 1);
+            Profiler.sectionStartFull.put(name, start);
         } else {
             Profiler.sectionCount.put(name, Profiler.sectionCount.get(name) + 1);
         }
-        Profiler.sectionStart.put(name, start);
+        Profiler.sectionStartSelf.put(name, start);
     }
     public static void exitSection(String name) {
         long finish = System.currentTimeMillis();
-        long sectionTime = finish - Profiler.sectionStart.get(name);
+        long sectionTime = finish - Profiler.sectionStartFull.get(name);
         Profiler.sectionFinish.put(name, finish);
         Profiler.sectionFullTime.put(name, (int) sectionTime);
     }
@@ -28,26 +30,21 @@ public class Profiler {
         // массив для имён секций
         String [] sftArr = Profiler.sectionFullTime.keySet().toArray(new String[Profiler.sectionFullTime.size()]);
         // вычисляем селфтайм
-        if (sftArr.length == 1) Profiler.sectionSelfTime.put(sftArr[0], sectionFullTime.get(sftArr[0]) * sectionCount.get(sftArr[0]));
-        for (int i = 1; i < sftArr.length; i++) {
-            if (i == 1) {
-                Profiler.sectionSelfTime.put(sftArr[i - 1], Profiler.sectionFullTime.get(sftArr[i - 1]) - Profiler.sectionFullTime.get(sftArr[i]) * Profiler.sectionCount.get(sftArr[i]));
-            } else {
-                if (Profiler.sectionFinish.get(sftArr[i]) > Profiler.sectionStart.get(sftArr[i - 1]) && Profiler.sectionFinish.get(sftArr[i]) < Profiler.sectionFinish.get(sftArr[i - 1])) {
-                    Profiler.sectionSelfTime.put(sftArr[i - 1], (Profiler.sectionFullTime.get(sftArr[i - 1]) * Profiler.sectionCount.get(sftArr[i - 1])) - Profiler.sectionFullTime.get(sftArr[i]));
-                    if (i == sftArr.length - 1) Profiler.sectionSelfTime.put(sftArr[i], Profiler.sectionFullTime.get(sftArr[i]) * Profiler.sectionCount.get(sftArr[i]));
-                } else {
-                    Profiler.sectionSelfTime.put(sftArr[0], Profiler.sectionSelfTime.get(sftArr[0]) - Profiler.sectionFullTime.get(sftArr[i]));
-                    Profiler.sectionSelfTime.put(sftArr[i - 1], Profiler.sectionFullTime.get(sftArr[i - 1]) * Profiler.sectionCount.get(sftArr[i - 1]));
-                    if (i == sftArr.length - 1) Profiler.sectionSelfTime.put(sftArr[i], Profiler.sectionFullTime.get(sftArr[i]) * Profiler.sectionCount.get(sftArr[i]));
-                }
+        if (sftArr.length == 1) {
+            Profiler.sectionSelfTime.put(sftArr[0], sectionFullTime.get(sftArr[0]));
+        } else {
+            // вычисляем фуллтайм и селфтайм для последней секции
+            sectionFullTime.put(sftArr[sftArr.length - 1], (int) (sectionFinish.get(sftArr[sftArr.length - 1]) - sectionStartSelf.get(sftArr[sftArr.length - 1])) * sectionCount.get(sftArr[sftArr.length - 1]));
+            sectionSelfTime.put(sftArr[sftArr.length - 1], sectionFullTime.get(sftArr[sftArr.length - 1]));
+            for (int i = 1; i < sftArr.length; i++) {
+                Profiler.sectionSelfTime.put(sftArr[i - 1], Profiler.sectionFullTime.get(sftArr[i - 1]) - Profiler.sectionFullTime.get(sftArr[i]));
             }
         }
         // заносим статистику в ArrayList
         for (int i = 0; i < sftArr.length; i++) {
             StatisticInfo statisticInfo = new StatisticInfo();
             statisticInfo.sectionName = sftArr[i];
-            statisticInfo.fullTime = Profiler.sectionFullTime.get(sftArr[i]) * Profiler.sectionCount.get(sftArr[i]);
+            statisticInfo.fullTime = Profiler.sectionFullTime.get(sftArr[i]);
             statisticInfo.selfTime = Profiler.sectionSelfTime.get(sftArr[i]);
             statisticInfo.count = Profiler.sectionCount.get(sftArr[i]);
             statInfo.add(statisticInfo);
@@ -55,19 +52,12 @@ public class Profiler {
         return statInfo;
     }
     public static void main(String[] args) {
-        for (int i = 1; i < 4; i++) {
-            Profiler.enterSection("1");
+            Profiler.enterSection("Process1");
             long startTest = System.currentTimeMillis();
             while (System.currentTimeMillis() - startTest < 100) {
 
             }
-            Profiler.exitSection("1");
-        }
-        System.out.println(Profiler.sectionCount);
-        System.out.println(Profiler.sectionStart);
-        System.out.println(Profiler.sectionFinish);
-        System.out.println(Profiler.sectionFullTime);
+            Profiler.exitSection("Process1");
         System.out.println(Profiler.getStatisticInfo());
-        System.out.println(Profiler.sectionSelfTime);
     }
 }
